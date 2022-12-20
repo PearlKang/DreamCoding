d96dc87c070d7142984089bee2165f7b2f1b4e37
BenKang <rksk1588@gmail.com>
2022-12-21 01:07:10 +0900

    2022.12.21

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,6 +1,10 @@
 "use strict";
 
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
 // arrow function
 document.addEventListener("scroll", () => {
   console.log(window.screenY);
+  console.log(navbarHeight);
 });
