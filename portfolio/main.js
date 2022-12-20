e1af08132e978b24ca7920ffb513bdcfa6155b10
BenKang <rksk1588@gmail.com>
2022-12-21 01:16:39 +0900

    2022.12.21
    JS: Navbar

--- portfolio/main.js
+++ portfolio/main.js
@@ -3,11 +3,9 @@
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
+// Make navbar transparent when it is on the top
 // arrow function
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  console.log(`navbarHeight: ${navbarHeight}`);
-
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
