10b977cee043a24e1b3eb6c8a0d4ea528bca3f22
BenKang <rksk1588@gmail.com>
2022-12-22 00:45:41 +0900

    2022.12.22

--- portfolio/main.js
+++ portfolio/main.js
@@ -12,3 +12,10 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", () => {
+  console.log("test");
+});
