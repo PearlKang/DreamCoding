f29ed0260ef320b12c6d8772a3b128fd661d668a
BenKang <rksk1588@gmail.com>
2022-12-24 02:55:38 +0900

    2022.12.24
    JS: Home transparent

--- portfolio/main.js
+++ portfolio/main.js
@@ -35,7 +35,7 @@ homeContactBtn.addEventListener("click", () => {
 });
 
 // Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector("#home");
+const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
